1e5dba2c0d8f14b0459b04735e65947cb0d86a58
grit96 <geraintwhite@gmail.com>
2013-10-12 23:10:18 +0100

    Added php mail() option

--- include/functions.php
+++ include/functions.php
@@ -24,23 +24,37 @@
 		return mysqli_fetch_assoc(query_DB("SELECT * FROM users WHERE {$name}='{$value}'"));
 	}
 	function email($to, $from, $subject, $message) {
-		require_once($GLOBALS['mailDir']);
-		$mail = new PHPMailer();
-		$mail->IsSMTP();
-		$mail->Host       = $GLOBALS['smtpHost'];
-		$mail->SMTPDebug  = 1;
-		$mail->SMTPAuth   = true;
-		$mail->SMTPSecure = $GLOBALS['mailSecurity'];
-		$mail->Host       = $GLOBALS['smtpHost'];
-		$mail->Port       = $GLOBALS['mailPort'];
-		$mail->Username   = $GLOBALS['mailUser']['email'];
-		$mail->Password   = $GLOBALS['mailPass'];
-		$mail->SetFrom($from['email'], "{$from['name']}");
-		$mail->AddReplyTo($from['email'], "{$from['name']}");
-		$mail->Subject    = $subject;
-		$mail->AltBody    = $GLOBALS['noHTML'];
-		$mail->MsgHTML($message);
-		$mail->AddAddress($to['email'], "{$to['name']}");
+		// require_once($GLOBALS['mailDir']);
+		// $mail = new PHPMailer();
+		// $mail->IsSMTP();
+		// $mail->Host       = $GLOBALS['smtpHost'];
+		// $mail->SMTPDebug  = 1;
+		// $mail->SMTPAuth   = true;
+		// $mail->SMTPSecure = $GLOBALS['mailSecurity'];
+		// $mail->Host       = $GLOBALS['smtpHost'];
+		// $mail->Port       = $GLOBALS['mailPort'];
+		// $mail->Username   = $GLOBALS['mailUser']['email'];
+		// $mail->Password   = $GLOBALS['mailPass'];
+		// $mail->SetFrom($from['email'], "{$from['name']}");
+		// $mail->AddReplyTo($from['email'], "{$from['name']}");
+		// $mail->Subject    = $subject;
+		// $mail->AltBody    = $GLOBALS['noHTML'];
+		// $mail->MsgHTML($message);
+		// $mail->AddAddress($to['email'], "{$to['name']}");
 
-		return $mail->Send();
+		// return $mail->Send();
+
+		$to = $to['email'];
+		$from = $from['email'];
+
+		$headers   = array();
+		$headers[] = "MIME-Version: 1.0";
+		$headers[] = "Content-type: text/plain; charset=iso-8859-1";
+		$headers[] = "To: {$to['name']} <{$to['email']}>";
+		$headers[] = "From: {$from['name']} <{$from['email']}>";
+		$headers[] = "Reply-To: {$from['name']} <{$from['email']}>";
+		$headers[] = "Subject: {$subject}";
+		$headers[] = "X-Mailer: PHP/".phpversion();
+
+		mail($to, $subject, $email, implode("\r\n", $headers));
 	}
